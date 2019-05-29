ad3a8d0312f384c16b079d8ab248b8665707eedb
Rikki Schulte <richard.j.schulte@gmail.com>
2019-05-29 03:22:56 -0400

    more tests for lib

--- src/__tests__/lib.spec.ts
+++ src/__tests__/lib.spec.ts
@@ -1,27 +1,192 @@
-import { expect } from "chai";
-import * as fs from "fs";
+import * as execa from 'execa'
+import * as mkdirp from 'mkdirp'
+import * as rimraf from 'rimraf'
 
-import { cloneProject, checkoutBranch } from "../lib";
-import rimraf = require("rimraf");
+jest.mock('execa')
 
-describe("cloneProject", () => {
-  it("should clone a project", () => {
-    cloneProject("https://github.com/acao/yarn-compose", "/tmp/example");
-    expect(fs.existsSync("/tmp/example/.git"));
-    rimraf.sync("/tmp/example/");
-  });
-});
+import {
+  cloneProject,
+  checkoutBranch,
+  installDependencies,
+  buildProject,
+  linkSelf,
+  linkDependencies,
+} from '../lib'
 
-describe("checkoutBranch", () => {
-  it("should checkout a branch in an existing repo", () => {
-    cloneProject("https://github.com/acao/yarn-compose", "/tmp/example");
-    checkoutBranch("/tmp/example", "d7674130d80c9396c8195101c69596a217c7cad9");
-    expect(!fs.existsSync("/tmp/example/src/commands"));
-    rimraf.sync("/tmp/example/");
-  });
-  it("should fail when a repository doesnt exist", () => {
-    expect(() => checkoutBranch("/tmp/example", "d7674130d80c9396c8195101c69596a217c7cad9")).to.throw(
-      "Cannot checkout d7674130d80c9396c8195101c69596a217c7cad9, /tmp/example is not a git repository"
-    );
-  });
-});
+const project = {
+  package: 'example',
+  branch: 'master',
+  remote: 'git@github.com:example/example.git',
+  links: ['another-example', 'another-example-1'],
+  types: ['example-type'],
+}
+
+const lernaProject = {
+  package: 'lerna-example',
+  branch: 'master',
+  remote: 'git@github.com:example/lerna-example.git',
+  lerna: true,
+  links: ['another-example', 'another-example-1'],
+}
+
+const DIR = '/tmp/example'
+
+describe('cloneProject', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+    rimraf.sync(DIR)
+  })
+
+  it('should clone a project', () => {
+    cloneProject('https://github.com/acao/yarn-compose', DIR)
+    expect(execa.sync).toHaveBeenCalledWith('git', [
+      'clone',
+      'https://github.com/acao/yarn-compose',
+      DIR,
+    ])
+  })
+
+  it('should not clone a project when it already exists', () => {
+    mkdirp.sync('/tmp/example/.git')
+    cloneProject('https://github.com/acao/yarn-compose', DIR)
+    expect(execa.sync).toHaveBeenCalledTimes(0)
+  })
+})
+
+describe('checkoutBranch', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+    rimraf.sync(DIR)
+  })
+
+  it('should checkout a branch in an existing repo', () => {
+    mkdirp.sync(DIR + '/.git')
+    checkoutBranch(DIR, 'd7674130d80c9396c8195101c69596a217c7cad9')
+    expect(execa.sync).toHaveBeenLastCalledWith(
+      'git',
+      ['checkout', 'd7674130d80c9396c8195101c69596a217c7cad9'],
+      {
+        cwd: DIR,
+      }
+    )
+  })
+
+  it('should fail when a repository doesnt exist', () => {
+    expect(() =>
+      checkoutBranch(DIR, 'd7674130d80c9396c8195101c69596a217c7cad9')
+    ).toThrowError(
+      'Cannot checkout d7674130d80c9396c8195101c69596a217c7cad9, /tmp/example is not a git repository'
+    )
+  })
+})
+
+describe('installDependencies', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should install packages for a normal repository', () => {
+    installDependencies(DIR, project, { countOf: [1, 1] })
+    expect(execa.sync).toHaveBeenLastCalledWith(
+      'yarn',
+      ['install', '--ignore-scripts'],
+      { cwd: DIR }
+    )
+  })
+
+  it('should install packages with --force flag', () => {
+    installDependencies(DIR, lernaProject, { countOf: [1, 1], force: true })
+    expect(execa.sync).toHaveBeenLastCalledWith(
+      'yarn',
+      ['install', '--ignore-scripts', '--force'],
+      {
+        cwd: DIR,
+      }
+    )
+  })
+})
+
+describe('buildProject', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should build project for a normal repository', () => {
+    buildProject(DIR, project, { countOf: [1, 1] })
+    expect(execa.sync).toHaveBeenLastCalledWith('yarn', ['build'], { cwd: DIR })
+  })
+
+  it('should build project for a lerna repository', () => {
+    buildProject(DIR, lernaProject, { countOf: [1, 1] })
+    expect(execa.sync).toHaveBeenLastCalledWith('lerna', ['run', 'build'], {
+      cwd: DIR,
+    })
+  })
+})
+
+describe('linkSelf', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should link normal project', () => {
+    linkSelf(DIR, project, { countOf: [1, 1] })
+    expect(execa.sync).toHaveBeenCalledWith('yarn', ['unlink'], { cwd: DIR })
+    expect(execa.sync).toHaveBeenLastCalledWith('yarn', ['link'], { cwd: DIR })
+  })
+
+  it('should link all projects in a lerna repository', () => {
+    linkSelf(DIR, lernaProject, { countOf: [1, 1] })
+    expect(execa.sync).toHaveBeenCalledWith(
+      'lerna',
+      ['exec', 'yarn', 'unlink'],
+      {
+        cwd: DIR,
+      }
+    )
+    expect(execa.sync).toHaveBeenLastCalledWith(
+      'lerna',
+      ['exec', 'yarn', 'link'],
+      {
+        cwd: DIR,
+      }
+    )
+  })
+
+  it('should build project and honor opts.project.linkFrom', () => {
+    linkSelf(DIR, { ...project, linkFrom: 'path' }, { countOf: [1, 1] })
+    expect(execa.sync).toHaveBeenLastCalledWith('yarn', ['link'], {
+      cwd: DIR + '/path',
+    })
+  })
+})
+
+describe('linkDependencies', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should link dependencies for a normal repository', () => {
+    linkDependencies(DIR, project, { countOf: [1, 1] })
+    expect(execa.sync).toHaveBeenLastCalledWith(
+      'yarn',
+      ['link', 'another-example', 'another-example-1'],
+      { cwd: DIR }
+    )
+  })
+
+  it('should link dependencies for a lerna repository', () => {
+    linkDependencies(DIR, lernaProject, { countOf: [1, 1] })
+    expect(execa.sync).toHaveBeenLastCalledWith(
+      'lerna',
+      ['exec', '--', 'yarn', 'link', 'another-example', 'another-example-1'],
+      {
+        cwd: DIR,
+      }
+    )
+  })
+  it('should do nothing if there are no links', () => {
+    linkDependencies(DIR, { ...project, links: undefined }, { countOf: [1, 1] })
+    expect(execa.sync).toHaveBeenCalledTimes(0)
+  })
+})
