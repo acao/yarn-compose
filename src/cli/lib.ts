7dc47311432c42e1b5375a3e2715d6a298a5f000
Rikki Schulte <richard.j.schulte@gmail.com>
2019-06-15 13:14:14 -0400

    re-add try/catch block

--- src/cli/lib.ts
+++ src/cli/lib.ts
@@ -12,39 +12,46 @@ import {
 import { logger } from '../util'
 
 export function runCLI(argv: string[]) {
-  const [, , commandArg] = argv
+  try {
+    const [, , commandArg] = argv
 
-  if (!commandArg) {
-    throw Error(`No command provided. Try: ${listAvailableCommands()}`)
+    if (!commandArg) {
+      throw Error(`No command provided. Try: ${listAvailableCommands()}`)
+    }
+    if (!commandMap[commandArg]) {
+      throw Error(
+        `Command ${chalk.whiteBright.bold(
+          commandArg
+        )} does not exist.\nTry: ${listAvailableCommands()}`
+      )
+    }
+
+    const SelectedCmd = commandMap[commandArg]
+
+    logger.meta(SelectedCmd.commandName)
+
+    const meowConfig = getMeowConfig(argv)
+
+    if (SelectedCmd.additionalFlags) {
+      meowConfig.flags = Object.assign(
+        meowConfig.flags,
+        SelectedCmd.additionalFlags
+      )
+    }
+
+    const meowResult = meow(SelectedCmd.commandHelp, meowConfig)
+
+    if (meowResult.flags.help || !commandArg) {
+      logger.help(SelectedCmd.commandHelp + '\n' + defaultHelp)
+      process.exit(0)
+    }
+
+    const Cmd = new SelectedCmd(meowResult.flags)
+    Cmd.run()
+  } 
+  catch (err) {
+    logger.error(err)
+    process.exit(1)
   }
-  if (!commandMap[commandArg]) {
-    throw Error(
-      `Command ${chalk.whiteBright.bold(
-        commandArg
-      )} does not exist.\nTry: ${listAvailableCommands()}`
-    )
-  }
 
-  const SelectedCmd = commandMap[commandArg]
-
-  logger.meta(SelectedCmd.commandName)
-
-  const meowConfig = getMeowConfig(argv)
-
-  if (SelectedCmd.additionalFlags) {
-    meowConfig.flags = Object.assign(
-      meowConfig.flags,
-      SelectedCmd.additionalFlags
-    )
-  }
-
-  const meowResult = meow(SelectedCmd.commandHelp, meowConfig)
-
-  if (meowResult.flags.help || !commandArg) {
-    logger.help(SelectedCmd.commandHelp + '\n' + defaultHelp)
-    process.exit(0)
-  }
-
-  const Cmd = new SelectedCmd(meowResult.flags)
-  Cmd.run()
 }
